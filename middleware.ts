851880bf3d84615f4e51abd709b2ebb929adc57f
chirayu-xx <cboss1436@gmail.com>
2023-04-21 16:53:04 +0530

    bug

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/chat/:id*", "/"]}
+export const config = { matcher: ["/chat/:id*"]}
 // matcher: ["/chat/:id*", "/"],
